c947977739ed32e034b1d57b7da59551fac725be
Mehmet karabulut <karabulutme20@itu.edu.tr>
2022-03-14 23:17:57 +0300

    python cod exercises

---
+++ python kodlama egzersizleri/ödev6_hipatenüs.py
@@ -0,0 +1,6 @@
+a =  float(input("lütfen dik kenarlardan birini giriniz "))
+b = float(input("lütfen dik kenarlardan diğerini giriniz "))
+
+hipatenüs = (a**2 + b**2 )  ** (1/2)
+
+print("hipatenüs =  {}".format(hipatenüs))
