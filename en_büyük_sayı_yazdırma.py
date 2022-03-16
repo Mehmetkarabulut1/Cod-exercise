d726f0ccb1beca882d11c70c5c95dc412a6afeea
Mehmet Karabulut <101456800+Mehmetkarabulut1@users.noreply.github.com>
2022-03-16 17:02:44 +0300

    Delete en_büyük_sayı_yazdırma.py

--- en_büyük_sayı_yazdırma.py
+++
@@ -1,6 +0,0 @@
-a = float(input("sayı1"))
-    print("en büyük sayı :{}".format(a))
-elif b>a and b>c:
-    print("en büyük sayı :{}".format(b))
-elif c>a and c>b:
-    print("en büyük sayı :{}".format(c))
