5fb4cdd326d700af70fcd285739b8af23710d69c
Mehmet Karabulut <101456800+Mehmetkarabulut1@users.noreply.github.com>
2022-03-16 17:03:33 +0300

    Add files via upload

---
+++ python kodlama egzersizleri/en_büyük_sayı_yazdırma.py
@@ -0,0 +1,6 @@
+a = float(input("sayı1"))
+    print("en büyük sayı :{}".format(a))
+elif b>a and b>c:
+    print("en büyük sayı :{}".format(b))
+elif c>a and c>b:
+    print("en büyük sayı :{}".format(c))
