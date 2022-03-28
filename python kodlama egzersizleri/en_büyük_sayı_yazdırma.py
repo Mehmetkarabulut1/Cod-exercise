0515f282482407afea2fe5a7a159d2340008c32e
Mehmet Karabulut <101456800+Mehmetkarabulut1@users.noreply.github.com>
2022-03-28 23:20:00 +0300

    Update en_büyük_sayı_yazdırma.py

--- python kodlama egzersizleri/en_büyük_sayı_yazdırma.py
+++ python kodlama egzersizleri/en_büyük_sayı_yazdırma.py
@@ -1,5 +1,7 @@
 a = float(input("sayı1"))
     print("en büyük sayı :{}".format(a))
+if a>b and a>c:
+    print("en büyük sayı :{}".format(b))
 elif b>a and b>c:
     print("en büyük sayı :{}".format(b))
 elif c>a and c>b:
