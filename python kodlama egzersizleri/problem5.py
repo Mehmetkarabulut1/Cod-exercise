83a4bbf97337a589c8cdca4a0878ea89cab3bcc0
Mehmet Karabulut <101456800+Mehmetkarabulut1@users.noreply.github.com>
2022-04-01 20:27:34 +0300

    Update problem5.py

--- python kodlama egzersizleri/problem5.py
+++ python kodlama egzersizleri/problem5.py
@@ -1,13 +1,17 @@
+#1'den 100'e kadar olan sayılardan sadece 3'e bölünen sayıları ekrana bastıran işlemler 3 farklı şekilde yazılmıştır.
+#1
 i = 0
-while i < 101:
+while i < 101: 
     if i%3 == 0:
         print(i)
     i += 1
 
+#2
 for i in range(0,101):
     if i%3 == 0:
         print(i)
 
+#3
 for i in range(0,101):
     if i%3 != 0:
         continue
